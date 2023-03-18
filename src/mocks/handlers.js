0c5dc3fd23d694f97f3192ef62bb6f03f0e05aaa
JureSeselj <jure.seselj@gmail.com>
2023-03-18 19:01:16 +0000

    Cleaning up errors

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,6 +1,6 @@
 import { rest } from "msw";
 
-const baseURL = "https://drf-api-rec.herokuapp.com/";
+const baseURL = "https://drf-api-new.herokuapp.com/";
 
 export const handlers = [
   rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
